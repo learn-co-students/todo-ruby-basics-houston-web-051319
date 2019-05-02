f2db410c8ac8306c3f78b202003e9e6c845d43e4
Brendan Brown <pbbrown1112@gmail.com>
2019-05-02 13:51:48 -0500

    Completed basics methods

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,27 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  x = value
 end
 
-def argue
-  
+def argue(phrase)
+  phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  "#{greeting}, #{name}"
 end
 
-def return_a_value
-
+def return_a_value(value= "Nice")
+  value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value1= "expert")
+  value1
 end
 
-def pizza_party
-
+def pizza_party(value2 = "cheese")
+  value2
 end
