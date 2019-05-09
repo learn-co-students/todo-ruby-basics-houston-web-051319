76a9217b459b2e7733ec77c2071a939cc55af34f
MARUF B AWAL <marufbinawal@gmail.com>
2019-05-09 13:41:03 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,12 +1,15 @@
-def division(num1, num2)
+def division(num1(42), num2(7) )
+  num1 / num2
  
 end
 
 def assign_variable(value)
+  value="Bob"
   
 end
 
-def argue
+def argue(phrase)
+  phrase="I'm right and you are wrong!"
   
 end
 
